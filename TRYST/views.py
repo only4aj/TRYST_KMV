5b527345873881bf4fbc159693ebb0e798aafa3c
only4aj <onlyforanshjuneja@gmail.com>
2024-02-22 01:18:39 +0530

    Partial Home page with Responsive NavBar

--- TRYST/views.py
+++ TRYST/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,redirect,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, Welcome to TRYST'24")
+    return render(request , 'home.html')
