5559bf42275544028d2752060d9c60637a436853
only4aj <onlyforanshjuneja@gmail.com>
2024-02-22 23:54:20 +0530

    Home Page - Partially Completed

--- TRYST/views.py
+++ TRYST/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,redirect,HttpResponse
 # Create your views here.
 def home(request):
     return render(request , 'home.html')
+
+def contact(request):
+    return render(request , "contact.html")
