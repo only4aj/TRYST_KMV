a7d6b69eaca740ea6900e3be0117515f7a008b87
only4aj <onlyforanshjuneja@gmail.com>
2024-02-24 12:25:55 +0530

    Backend - Models.py

--- TRYST/models.py
+++ TRYST/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ContactForm(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField()
+    desc = models.CharField(max_length=5000)
+
+    def __str__(self) -> str:
+        return self.fname
