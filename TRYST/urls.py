1eb8fda0dd5e961e5ecb44dd5f20317668d2c49a
only4aj <onlyforanshjuneja@gmail.com>
2024-02-24 12:27:02 +0530

    Mapped Urls in Backend

--- TRYST/urls.py
+++ TRYST/urls.py
@@ -4,4 +4,5 @@ from TRYST import views
 urlpatterns = [
     path('' , views.home , name = 'home'),
     path('contact/' , views.contact , name = 'contact'),
+    path('events/' , views.events , name = "events"),
 ]
