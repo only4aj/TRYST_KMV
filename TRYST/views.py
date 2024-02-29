df4858e6c50a042bc0d0ffaa1727e1e2630dc515
only4aj <onlyforanshjuneja@gmail.com>
2024-02-29 21:39:58 +0530

    migrations

--- TRYST/views.py
+++ TRYST/views.py
@@ -1,46 +1,15 @@
-<<<<<<< HEAD
-from django.shortcuts import render,redirect,HttpResponse
-from .models import ContactForm
-=======
 from django.shortcuts import render, redirect, HttpResponse
 from .models import RegistrationForm
 from django.core.mail import send_mail
 from django.conf.global_settings import EMAIL_HOST_USER,EMAIL_HOST_PASSWORD
 
 
->>>>>>> b6a570df02420777921a8fccab55259975563313
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 
 def contact(request):
-<<<<<<< HEAD
-
-    message = None
-    clear_form = False
-
-    if request.method == 'POST':
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
-        usremail = request.POST.get('usremail')
-        description = request.POST.get('description')
-
-        if not (firstname and lastname and usremail and description):
-            message = 'Please fill out all fields.'
-        else:
-            contactdata = ContactForm(fname=firstname, lname=lastname, email=usremail, desc=description)
-            contactdata.save()
-            message = 'Thanks for contacting us! We will reach you soon...'
-            clear_form = True
-
-    return render(request, 'contact.html', {'message': message, 'clear_form': clear_form})
-
-
-
-def events(request):
-    return render(request , "events.html")
-=======
     firstname = request.POST.get("firstname","")
     lastname = request.POST.get("lastname","")
     usermail = request.POST.get("usremail","")
@@ -95,4 +64,3 @@ def send_email(subject,message,receiver):
     recipient_list = [receiver]
     
     send_mail(subject , message , from_email , recipient_list)
->>>>>>> b6a570df02420777921a8fccab55259975563313
